4355bec34890d592957fa33e97064504acffea65
1by4piepsilonot <hkg332211@gmail.com>
2026-04-16 12:45:22 +0530

    added new feature of viewing problems

--- main.cpp
+++ main.cpp
@@ -15,13 +15,28 @@ void addProblem() {
 
     cout << "Saved!\n";
 }
+void viewProblems() {
+    ifstream file("problems.txt");
+    string line;
+
+    cout << "\n--- Problem List ---\n";
+
+    while (getline(file, line)) {
+        cout << line << endl;
+    }
+
+    file.close();
+}
 
 int main() {
     int choice;
+
     cout << "1. Add Problem\n";
+    cout << "2. View Problems\n";
     cin >> choice;
 
     if (choice == 1) addProblem();
+    else if (choice == 2) viewProblems();
 
     return 0;
 }
